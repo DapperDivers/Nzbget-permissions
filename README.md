58541ad73706d2c193f0ba3fa936b9de9a372cf3
dapperdivers <derek.mackley@hotmail.com>
2024-06-10 09:32:51 -0500

    init repo

---
+++ README.md
@@ -0,0 +1,10 @@
+# NZBGet Versions
+
+    stable v23+ v3.0
+    legacy v22 v2.1
+
+# Permissions
+
+The intent of this script is to modify the permissions in the downloads directory during the post processing step.
+
+Author: dapperdivers
---
+++ main.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+###########################################
+
+### NZBGET POST-PROCESSING SCRIPT       ###
+
+# Modifies permissions for a directory
+
+#
+
+# The intent of this script is to change the group permissions
+
+# for the downloads directory to something different such that
+
+# downstream processing will work.
+
+########################################
+### OPTIONS                          ###
+
+# Group
+#
+# Specify the group that you'd like the files to be changed to
+Group=Media
+
+### NZBGET POST-PROCESSING SCRIPT       ###
+
+###########################################
+
+chown -R nzbget:{Group} *
